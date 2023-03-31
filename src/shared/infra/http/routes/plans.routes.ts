0cf05bf95adb50d5f0cac0a180b659e3563e17dc
RaphaelMW <raphael@mundowap.com.br>
2023-03-31 11:17:26 -0300

    chore: list all plans

--- src/shared/infra/http/routes/plans.routes.ts
+++ src/shared/infra/http/routes/plans.routes.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 import { CreatePlanController } from "../../../../modules/Plans/useCase/createPlan/CreatePlanController";
 import { GetPlanByIdController } from "../../../../modules/Plans/useCase/getPlanById/GetPlanByIdController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ListPlansController } from "../../../../modules/Plans/useCase/ListPlans/ListPlansController";
 
 const planRoute = Router();
 
 const createPlanController = new CreatePlanController();
 const getPlanByIdController = new GetPlanByIdController();
+const listPlansController = new ListPlansController();
 
 planRoute.post("/", ensureAuthenticated, createPlanController.handle);
 planRoute.get("/:id", ensureAuthenticated, getPlanByIdController.handle);
+planRoute.get("/", ensureAuthenticated, listPlansController.handle);
 
 export { planRoute };
