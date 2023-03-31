6cc714e4cfb58e0c3e3addf99496f15a57afb68b
RaphaelMW <raphael@mundowap.com.br>
2023-03-31 14:10:37 -0300

    chore: add payment after actual payment is paid

--- src/modules/Payments/useCase/ConfirmPayment/ConfirmPaymentController.ts
+++ src/modules/Payments/useCase/ConfirmPayment/ConfirmPaymentController.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { ConfirmPaymentUseCase } from "./ConfirmPaymentUseCase";
+import { CreatePaymentUseCase } from "../CreatePayment/CreatePaymentUseCase";
 
 export class ConfirmPaymentController {
   async handle(request: Request, response: Response) {
     const { pix } = request.body;
     const confirmPaymentUseCase = container.resolve(ConfirmPaymentUseCase);
 
-    pix.map(async (item: any) => await confirmPaymentUseCase.execute(item));
+    pix.map(async (item: any) => {
+      const payment = await confirmPaymentUseCase.execute(item);
+
+      const createPaymentUseCase = container.resolve(CreatePaymentUseCase);
+
+      await createPaymentUseCase.execute({
+        clientId: payment.clientId,
+        plansId: payment.plansId,
+        date: payment.dueDate
+      });
+    });
 
     response.status(200).send();
   }
